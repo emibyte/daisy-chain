6e1cb3ec379535282344147b20ab816b0cbb2f31
emibyte <fevymarine@gmail.com>
2025-10-05 20:21:52 +0200

    just some base stuff to test the nix build with

---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <curses.h>
+
+int main(void) {
+  printf("this lowkey kinda works\n");
+  return 0;
+}
---
+++ src/test.c
@@ -0,0 +1,8 @@
+// #include "../includes/munit/munit.h"
+#include "../includes/munit/munit.h"
+#include <stdio.h>
+
+int main(void) {
+  printf("this is the test file\n.");
+  return 0;
+}

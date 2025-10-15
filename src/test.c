cbafcae0fdaa66c5215be16e72c5758f795ffbab
emibyte <fevymarine@gmail.com>
2025-10-15 20:49:09 +0200

    tests for task_repr and task_node

--- src/test.c
+++ src/test.c
@@ -1,14 +1,16 @@
+#include "chain.h"
 #include "munit/munit.h"
 #include "task.h"
+#include <string.h>
 
-int main(void) {
-  return 0;
-}
+int main(void) { return 0; }
 
-MunitResult test_new_task(const MunitParameter params[], void *user_data_or_fixture) {
+MunitResult test_new_task(const MunitParameter params[],
+                          void *user_data_or_fixture) {
   int seconds_day = 60 * 60 * 24;
   time_t current_time = time(NULL);
-  task_t *task = new_task(4, "clean up my desk", current_time + seconds_day, MEDIUM, NULL, 0);
+  task_t *task = new_task(4, "clean up my desk", current_time + seconds_day,
+                          MEDIUM, NULL, 0);
 
   munit_assert_ptr_not_null(task);
   munit_assert_int(task->id, ==, 4);
@@ -24,13 +26,16 @@ MunitResult test_new_task(const MunitParameter params[], void *user_data_or_fixt
   return MUNIT_OK;
 };
 
-MunitResult test_edit_task(const MunitParameter params[], void *user_data_or_fixture) {
-  int seconds_day = 60 * 60 * 24;
+MunitResult test_edit_task(const MunitParameter params[],
+                           void *user_data_or_fixture) {
+  long seconds_day = 60 * 60 * 24;
   time_t current_time = time(NULL);
   time_t initial_due_date = current_time + seconds_day;
-  task_t *task = new_task(4, "clean up my desk", initial_due_date, MEDIUM, NULL, 0);
-  edit_task(task, "this is a changed description", task->due_date + 10 * seconds_day, HIGH, NULL);
-  
+  task_t *task =
+      new_task(4, "clean up my desk", initial_due_date, MEDIUM, NULL, 0);
+  edit_task(task, "this is a changed description",
+            task->due_date + 10 * seconds_day, HIGH, NULL);
+
   munit_assert_ptr_not_null(task);
   munit_assert_string_not_equal(task->description, "clean up my desk");
   munit_assert_string_equal(task->description, "this is a changed description");
@@ -42,3 +47,46 @@ MunitResult test_edit_task(const MunitParameter params[], void *user_data_or_fix
 
   return MUNIT_OK;
 };
+
+MunitResult test_task_repr(const MunitParameter params[],
+                           void *user_data_or_fixture) {
+  long seconds_day = 60 * 60 * 24;
+  time_t current_time = time(NULL);
+  task_t *task = new_task(4, "clean up my desk", current_time + seconds_day,
+                          MEDIUM, NULL, 0);
+
+  char *expected = "4. - clean up my desk - [ ]";
+  char *actual = task_repr(task);
+
+  munit_assert_string_equal(expected, actual);
+
+  free_task(task);
+
+  return MUNIT_OK;
+}
+
+MunitResult test_new_task_node(const MunitParameter params[], void *user_data_or_fixture) {
+  long seconds_day = 60 * 60 * 24;
+  time_t current_time = time(NULL);
+  task_t *task = new_task(4, "clean up my desk", current_time + seconds_day,
+                          MEDIUM, NULL, 0);
+  task_node_t *node = new_task_node(NULL, task);
+
+  munit_assert_ptr_not_null(node->val);
+  munit_assert_ptr_null(node->next);
+
+  free_task_node(node);
+
+  return MUNIT_OK;
+}
+
+MunitResult test_new_chain(const MunitParameter params[], void *user_data_or_fixture) {
+  task_chain_t *chain = new_chain();
+
+  munit_assert_int(chain->size, ==, 0);
+  munit_assert_ptr_null(chain->head);
+
+  free_chain(chain);
+  
+  return MUNIT_OK;
+}

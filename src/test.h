51af97966066295eb9467694c47e06187387b231
emibyte <fevymarine@gmail.com>
2025-10-16 19:08:41 +0200

    header file for tests

---
+++ src/test.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "munit/munit.h"
+
+MunitResult test_new_task(const MunitParameter params[],
+                          void *user_data_or_fixture);
+MunitResult test_edit_task(const MunitParameter params[],
+                           void *user_data_or_fixture);
+MunitResult test_task_repr(const MunitParameter params[],
+                           void *user_data_or_fixture);
+
+MunitResult test_new_task_node(const MunitParameter params[],
+                               void *user_data_or_fixture);
+MunitResult test_new_chain(const MunitParameter params[],
+                           void *user_data_or_fixture);
+MunitResult test_add_task(const MunitParameter params[],
+                          void *user_data_or_fixture);
+MunitResult test_delete_task(const MunitParameter params[],
+                             void *user_data_or_fixture);
+MunitResult test_find_task(const MunitParameter params[], void
+*user_data_or_fixture);
